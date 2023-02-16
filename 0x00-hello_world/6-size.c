cf3ac27ecadfd776482a8d9f9073b54536893ce3
Omari <bkwadwoomari@gmail.com>
2023-02-16 23:13:34 +0000

    Program that prints the size of various C types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: 'C program that prints the size of various data types'
+ *
+ *Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+	int intergerType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %ld bytes\n",sizeof(integerType));
+
+	printf("Size of float: %ld bytes\n",sizeof(floatType));
+
+	printf("Size of double: %ld bytes\n",sizeof(doubleType));
+
+	printf("Size of char: %ld bytes\n",sizeof(charType));
+
+	return (0);
+}
