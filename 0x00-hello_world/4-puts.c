8278f24f72b474f13568b0a4c58443bf990963a9
Omari <bkwadwoomari@gmail.com>
2023-02-16 19:28:23 +0000

    Print with puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }
