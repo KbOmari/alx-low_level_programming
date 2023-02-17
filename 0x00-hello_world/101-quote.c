5e73a397beb73d5bb7e4420b4e845baa983e097a
Omari <bkwadwoomari@gmail.com>
2023-02-17 20:23:08 +0000

    UNIX genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 
 {
-	scanf("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
 	return (1);
 }
