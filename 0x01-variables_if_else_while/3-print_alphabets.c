57bb2437cb19f41877fe2b29eec091e5fe596784
Omari <bkwadwoomari@gmail.com>
2023-02-18 03:46:13 +0000

    Upper case then lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always (0) Success
+ *
+ */
+
+int main(void)
+
+{
+
+{
+	char c;
+
+	for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+	return (0);
+}
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
+
+}
