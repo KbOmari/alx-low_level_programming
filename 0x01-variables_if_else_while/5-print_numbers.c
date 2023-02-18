bcc73ccf6a1a24ee18eabb6022aa10dc6259b1da
Omari <bkwadwoomari@gmail.com>
2023-02-18 04:49:13 +0000

    0 - 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always (0) Success
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '1'; c <= '9'; c++)
+
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
