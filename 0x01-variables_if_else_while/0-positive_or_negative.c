276c3df45b99ef89ee4c5e5afdb7fa0ea0f98669
Omari <bkwadwoomari@gmail.com>
2023-02-18 01:53:15 +0000

    Positive negative neutral

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Determine whether a n is positive or negative or equal to zero
+ *
+ *Return: Always (0) Success
+ */
+
+int main(void)
+
+
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	printf("n is positive\n");
+	}
+	else if (n == 0)
+	{
+	printf("n is zero\n");
+	}
+	else
+	{
+	printf("n is negative\n");
+	}
+	return (0);
+}
