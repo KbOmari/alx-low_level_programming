5001319ab44a21f4dd10bad070cba54a2f3f56ba
Omari <bkwadwoomari@gmail.com>
2023-02-18 17:55:03 +0000

    Break in line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - prints exactly to the standard error
  *
- *Return: Always (0) Success
+ *Return: (1)
  */
+
 
 int main(void)
 
+
 {
-	fprintf(stderr, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,"
+
+	"2015-10-19\n");
 	return (1);
 }
