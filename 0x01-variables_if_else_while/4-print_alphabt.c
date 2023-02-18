5221bed76480393d2329b5f5cc137d68e90c6656
Omari <bkwadwoomari@gmail.com>
2023-02-18 04:38:10 +0000

    Leave out some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always (0) Success
+ *
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	if (c != 'q' && c != 'e')
+
+	putchar(c);
+
+	putchar('\n');
+	return (0);
+}
