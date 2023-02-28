d0c4098b2315939c0d0dae8418aecd072b017d42
Omari <bkwadwoomari@gmail.com>
2023-02-27 17:24:59 -0800

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: string
+ *
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+	int o;
+	while (*s != '\0')
+	{
+		x++;
+		s++;
+	}
+	s--;
+
+	for (o = x; o > 0; o--)
+
+		{
+		_putchar(*s);
+		s--;
+		}
+
+	_putchar('\n');
+}
