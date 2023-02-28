aabba22b98595327c6e1c401576256b10a3c9a07
Omari <bkwadwoomari@gmail.com>
2023-02-27 16:29:23 -0800

    Updates

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *reset_to_98 - entry point
+ *
+ *Description: program that takes a pointer to an int as parameter
+ *and updates the value it points to to 98
+ *
+ *@n: integer
+ *
+ *Return: int *n
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void reset_to_98(int *n);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif /* MAIN_H */
+
+
+
+
