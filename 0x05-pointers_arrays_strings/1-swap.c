98391c902fa0ae6bf9ac903f8dcfa842ab4b6e7a
Omari <bkwadwoomari@gmail.com>
2023-02-27 16:36:46 -0800

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps a and b
+ *
+ * @a: pointer 1
+ *
+ * @b: pointer 2
+ *
+ * Return; 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
