26be6f8222d7b2e86c6f1e36f54e1bbae72c4af3
KbOmari <bkwadwoomari@gmail.com>
2023-03-08 23:06:12 +0000

    cp memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: char array 
+ * @src: char array 
+ * @n: no of elements to copy
+ * Return: `dest`
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count = 0;
+
+	while (count < n)
+
+	{
+		dest[count] = src[count];
+		count++;
+	}
+
+	return (dest);
+}
