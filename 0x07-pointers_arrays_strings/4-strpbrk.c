42d9b2a2d543e45f00ecc56bd7636e1709241d77
KbOmari <bkwadwoomari@gmail.com>
2023-03-08 23:21:04 +0000

    Search string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Search a string
+ * @s: string
+ * @accept: string to match
+ * Return:  `s`, `accept` or NULL.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}
