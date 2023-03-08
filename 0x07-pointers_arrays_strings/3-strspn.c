ccc2be896f95a4f00a69cf85397087cba3451335
KbOmari <bkwadwoomari@gmail.com>
2023-03-08 23:17:56 +0000

    length

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - string for a set of bytes
+ * @s: char string array
+ * @accept: char array
+ * Return: Number of bytes in segment `s`
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int c;
+
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+			break;
+		i++;
+	}
+	return (c);
+}
