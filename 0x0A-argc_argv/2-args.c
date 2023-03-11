569d6fcc2ea936f0249b67afa2f483646724af37
KbOmari <bkwadwoomari@gmail.com>
2023-03-11 05:19:03 +0000

    Democracy

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - prints argument counts and strings
+ *
+ *@argc: Prints argument count
+ *@argv: prints argument strings
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	if (argc > 1)
+	{
+
+		for (x = 0; x < argc; x++)
+	{
+
+		printf("%s\n", argv[x]);
+
+	}
+
+	}
+	return (0);
+}
