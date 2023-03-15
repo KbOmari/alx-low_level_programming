853e881f92f75e17ee1df693b595d810af2bcafd
KbOmari <bkwadwoomari@gmail.com>
2023-03-15 17:18:14 +0000

    Multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *main - program starts
+ *
+ *@argc: prints argument count
+ *@argv: prints argument string
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int y;
+	int x;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else
+	{
+
+		int y = atoi(argv[1]);
+		int x = atoi(argv[2]);
+
+		printf("%d\n", y * x);
+	}
+	return (0);
+}
